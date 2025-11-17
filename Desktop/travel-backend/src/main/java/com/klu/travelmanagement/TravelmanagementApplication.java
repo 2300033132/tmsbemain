fecb0c162f9776b41a98f94041876d57084f70d3
2300033132 <2300033132@kluniversity.in>
2025-11-17 14:29:33 +0530

    Updated backend project

--- Desktop/travel-backend/src/main/java/com/klu/travelmanagement/TravelmanagementApplication.java
+++ Desktop/travel-backend/src/main/java/com/klu/travelmanagement/TravelmanagementApplication.java
@@ -2,13 +2,19 @@ package com.klu.travelmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelmanagementApplication {
+public class TravelmanagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelmanagementApplication.class, args);
-		System.out.println("tmsbe");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TravelmanagementApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmanagementApplication.class, args);
+        System.out.println("tmsbe");
+    }
 }
